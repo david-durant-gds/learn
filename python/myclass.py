95cf9cba3f8d4c97abd83e6aff59c47e6c78cba1
Jamie Atkinson <jamie.atkinson@digital.cabinet-office.gov.uk>
2017-03-13 13:57:13 +0000

    added

---
+++ python/myclass.py
@@ -0,0 +1,25 @@
+class busses:
+   'bus routes'
+
+   def __init__(self, route, rounds):
+      self.route = route
+      self.rounds = rounds
+      buscount.empCount += 1
+stop1 = 7
+stop2 = 3
+stop3 = 5
+stop4 = 2
+stop5 = 10
+route64 = [stop1,stop2,stop3,stop4,stop5]
+route32 = [stop1,stop2,stop4]
+sumroute64 = sum(route64)
+sumroute32 = sum(route32)
+
+def displayCount(self):
+    print "no of busses %d" % buscount.empCount
+
+def displaybus(self):
+    print "Route : ", self.route,  ", Rounds: ", self.rounds
+
+def routetime(self):
+    print "timefor route %d" % sumroute64
